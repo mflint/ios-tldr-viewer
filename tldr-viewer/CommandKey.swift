d6cf48730a71b3a15a59ea420c078ec6fd5afc1b
Matthew Flint <m@tthew.org>
2019-07-28 22:27:14 +0100

    Creates CommandKey, as a way of identifying a CommandVariant

---
+++ tldr-viewer/CommandKey.swift
@@ -0,0 +1,46 @@
+//
+//  CommandKey.swift
+//  tldr-viewer
+//
+//  Created by Matthew Flint on 28/07/2019.
+//  Copyright © 2019 Green Light. All rights reserved.
+//
+
+import Foundation
+
+struct CommandKey{
+    /// the name of the command
+    let commandName: String
+    
+    /// the name of the platform
+    let platformName: String?
+}
+
+extension CommandKey: ExpressibleByStringLiteral {
+    private static let separator: Character = "🦌"
+    
+    public init(stringLiteral value: String) {
+        let components = value.split(separator: CommandKey.separator)
+        
+        guard let commandName = components.first else {
+            preconditionFailure("no command name in CommandKey value")
+        }
+        self.commandName = String(commandName)
+        
+        platformName = components.count > 1 ? String(components[1]) : nil
+    }
+}
+
+extension CommandKey: CustomStringConvertible {
+    var description: String {
+        get {
+            if let platformName = platformName {
+                return "\(commandName)\(CommandKey.separator)\(platformName)"
+            }
+            
+            return commandName
+        }
+    }
+}
+
+extension CommandKey: Codable, Equatable {}
